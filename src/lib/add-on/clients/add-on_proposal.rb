8f75856475c80caaadad6dab23bb8d32dd67e59e
David Díaz <diazglezdavid@gmail.com>
2018-06-27 00:10:35 +0100

    WIP: Refactor AddOnProposal client

--- src/lib/add-on/clients/add-on_proposal.rb
+++ src/lib/add-on/clients/add-on_proposal.rb
@@ -3,10 +3,10 @@
 module Yast
   class AddOnProposalClient < Client
     def main
-      Yast.import "UI"
-      Yast.import "Pkg"
       textdomain "add-on"
 
+      Yast.import "UI"
+      Yast.import "Pkg"
       Yast.import "Label"
       Yast.import "Wizard"
       Yast.import "AddOnProduct"
@@ -14,66 +14,69 @@ module Yast
 
       Yast.include self, "add-on/add-on-workflow.rb"
 
-      @func = Convert.to_string(WFM.Args(0))
-      @param = Convert.to_map(WFM.Args(1))
-      @ret = {}
+      func, param = Yast::WFM.Args
+      log.info "Called #{self.class}.run with #{func} and params #{param}"
 
-      if @func == "MakeProposal"
-        @force_reset = Ops.get_boolean(@param, "force_reset", false)
-        @language_changed = Ops.get_boolean(@param, "language_changed", false)
-
-        @items = Builtins.maplist(AddOnProduct.add_on_products) do |product|
-          data = Pkg.SourceGeneralData(Ops.get_integer(product, "media", -1))
-          # placeholder for unknown path
-          dir = Ops.get_locale(data, "product_dir", _("Unknown"))
-          dir = "/" if dir == ""
-          # summary item, %1 is product name, %2 media URL, %3 directory on media
-          Builtins.sformat(
-            "%1 (Media %2, directory %3)",
-            Ops.get_string(product, "product", ""),
-            Ops.get_locale(data, "url", _("Unknown")),
-            dir
-          )
-        end
-        if Builtins.size(@items) == 0
-          # summary string
-          @items = [_("No add-on product selected for installation")]
-        end
-
-        WorkflowManager.RedrawWizardSteps
-
-        @ret = { "raw_proposal" => @items }
-      elsif @func == "AskUser"
-        Wizard.CreateDialog
-        @result = RunAddOnMainDialog(
-          false,
-          true,
-          true,
-          Label.BackButton,
-          Label.OKButton,
-          Label.CancelButton,
-          false
-        )
-        UI.CloseDialog
-
-        # Fill return map
-
-        @ret = { "workflow_sequence" => @result, "mode_changed" => false }
-      elsif @func == "Description"
-        # Fill return map.
-        #
-        # Static values do just nicely here, no need to call a function.
-
-        @ret = {
-          # this is a heading
-          "rich_text_title" => _("Add-On Products"),
-          # this is a menu entry
-          "menu_title"      => _("Add-&on Products"),
-          "id"              => "add_on"
-        }
+      case func
+      when "MakeProposal"
+        make_proposal
+      when "AskUser"
+        ask_user
+      when "Description"
+        description
+      else
+        raise ArgumentError, "Invalid action for Add-on proposal client '#{func.inspect}'"
       end
+    end
 
-      deep_copy(@ret)
+  private
+
+    def make_proposal
+      @items =
+        if AddOnProducts.add_on_products.empty?
+          [_("No add-on product selected for installation")]
+        else
+          AddOnProducts.add_on_products.map do |add_on|
+            media = add_on.fetch("media", -1)
+            general_data = Pkg.SourceGeneralData(media)
+            product = add_on.fetch("product", "")
+            product_dir = general_data.fetch("product_dir") { _("Unknown") }
+            product_dir = "/" if product_dir.empty?
+            url = general_data.fetch("url") { _("Unknown") }
+
+            "#{product} (Media #{url}, directory #{product_dir})"
+          end
+        end
+
+      WorkflowManager.RedrawWizardSteps
+
+      { "raw_proposal" => @items }
+    end
+
+    def ask_user
+      Wizard.CreateDialog
+
+      sequence = RunAddOnMainDialog(
+        false,
+        true,
+        true,
+        Label.BackButton,
+        Label.OKButton,
+        Label.CancelButton,
+        false
+      )
+
+      Wizard.CloseDialog
+
+      { "workflow_sequence" => sequence, "mode_changed" => false }
+    end
+
+    def description
+      {
+        "id"              => "add_on",
+        "rich_text_title" => _("Add-On Products"), # heading
+        "menu_title"      => _("Add-&on Products") # menu entry
+      }
     end
   end
 end
